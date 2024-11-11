03f4cf5fbcc8b027798dd4af58adc7611fb4c457
Jack DeSive <jackdesive17@gmail.com>
2024-11-11 17:09:36 -0600

    Init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./views/dashboard/dashboard.component";
+import {LayoutComponent} from "./layout/layout/layout.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {path: 'dashboard', loadComponent: () => DashboardComponent},
+      {path: '', redirectTo: '/dashboard', pathMatch: "full"}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
